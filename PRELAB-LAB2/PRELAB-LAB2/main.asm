ddb984571464f395d290fdf868f43e5647565fea
pedroexpert2000 <porras.pedro.pablo@gmail.com>
2026-02-11 12:42:56 -0600

    PRELAB-LAB2
    TIMER0 EN 4 LEDS

---
+++ PRELAB-LAB2/PRELAB-LAB2/main.asm
@@ -0,0 +1,136 @@
+/*
+* 
+*
+* Autor : Pedro Pablo Porras
+* Descripción: LABORATORIO 2: BOTONES Y TIMER0 
+*/
+
+.include "M328PDEF.inc"
+
+/****************************************/
+.cseg
+.org 0x0000
+
+/****************************************/
+/* CONFIGURACIÓN DE LA PILA */
+
+RESET:
+    LDI     R16, LOW(RAMEND)
+    OUT     SPL, R16
+    LDI     R16, HIGH(RAMEND)
+    OUT     SPH, R16
+
+/****************************************/
+/* SETUP */
+
+    LDI     R16, 0b00011111    // Definimos entradas y salidas en PORTB
+    OUT     DDRB, R16
+
+    LDI     R16, 0b00000000    // Definimos entradas y salidas en PORTC
+    OUT     DDRC, R16
+
+	LDI     R16, 0b11111111    // Definimos entradas y salidas en PORTD
+	OUT     DDRD, R16
+
+	LDI     R16, 0b00011111    // Activamos las salida de las LEDs
+	OUT     PORTB, R16 
+
+    SBI     PORTC, PB0         // ACTIVAMOS PULL UP BOTON 1
+    SBI     PORTC, PB1         // ACTIVAMOS PULL UP BOTON 2 
+
+	LDI     R16, 0b11111111    // Activamos las salidas de las LEDs para display 
+	OUT     PORTD, R16 
+
+    CLR     R21                // CONTADOR  A UTILIZAR
+
+	LDI     R16, 0x00          // APAGAMOS LA COMUNICACION SERIAL DE PINEB
+	STS     UCSR0B, R16 
+
+	LDI		R16, (1<<CLKPCE)   // MODIFICAMOS EL PRESCALER A 1MHZ
+    STS		CLKPR, R16
+    LDI		R16, 0b00000100
+    STS		CLKPR, R16
+
+	LDI     R16, (1<<CS02)    // TIMER 0 (256 PRESCALER)
+    OUT     TCCR0B, R16
+
+
+/****************************************/
+/* LOOP PRINCIPAL */
+
+MAIN_LOOP:
+    
+    IN      R16, TIFR0 
+    RCALL   WAIT_OVF              // MANDAMOS A LLAMAR EL TIMER0 
+
+	INC     R21                 // INCREMENTAMOS EL REGISTRO
+	ANDI    R21, 0X0F           // TOMAMOS EL NIBBLE MENOS SIGNIFICATIVO
+	MOV     R16, R21            // MOVEMMOS EL CONTAD0R A UN NUEVO REGISTRO 
+	OUT     PORTB, R16          // SACAMOS EL NUEVO REGISTRO A PORTB
+	RJMP    MAIN_LOOP
+
+
+WAIT_OVF:
+WAIT:
+    IN     R16, TIFR0            // LEEMOS BANDERAS
+    SBRS   R16, TOV0             // VERIFICAMOS OVERFLOW 
+    RJMP   WAIT      
+
+    SBI    TIFR0, TOV0           //  LIMPIAMOS BANDERA
+    RET
+
+
+
+/*
+	IN      R20, PINC       // LEEMOS PIN C 
+
+    SBRS    R20, 0          // VERIFICAMOS QUE EL PIN0 SE ENCUENTRE PRESIONADO (0)
+    RCALL   SUMAR           // LLAMAMOS A LA FUNCION SUMAR
+
+    SBRS    R20, 1          // VERIFICAMOS QUE EL PIN1 SE ENCUENTRE PRESIONADO (0)
+    RCALL   RESTAR          // LLAMAMOS A LA FUNCION RESTAR 
+
+    MOV     R16, R21        // MOVEMOS EL CONTADOR A NUESTRO VECTOR DE BITS
+    ANDI    R16, 0x0F       // TOMAMOS LOS 4 BITS MENOS SIGNFICATIVOS PARA GENERAR LAS 16 COMB. 
+    OUT     PORTB, R16      // MOSTRAMOS LA SALIDA EN PUERTO B 
+
+    RJMP    MAIN_LOOP       // SALTO RELATIVO A LOOP PRINCIPAL
+
+
+// SUBRUTINAS 
+
+
+SUMAR:             
+    INC     R21             // INCREMENTAMOS REGISTRO 
+    ANDI    R21, 0x0F       // LIMITAMOS EL CONTADOR CON LOS 4 ULTIMOS BITS
+    RCALL   DELAY           
+ESPERA_PC0:
+    IN      R20, PINC       // LEEMOS NUEVAMENTE PINC 
+    SBRS    R20, 0          //  PC0 ESTA EN 0 
+    RJMP    ESPERA_PC0       
+    RET
+
+RESTAR:
+    DEC     R21
+    ANDI    R21, 0x0F
+    RCALL   DELAY
+ESPERA_PC1:
+    IN      R20, PINC
+    SBRS    R20, 1
+    RJMP    ESPERA_PC1
+    RET
+
+
+ DELAY (ANTIREBOTE) 
+
+DELAY:
+    LDI     R18, 255
+D1:
+    LDI     R19, 255
+D2:
+    DEC     R19
+    BRNE    D2
+    DEC     R18
+    BRNE    D1
+    RET
+	*/
